9a8db4e0dccd7f0474dfca1b1e4d9e4b80999afb
jlauza <lauzajayson@gmail.com>
2025-10-24 02:47:09 +0800

    removed htaccess added toml

---
+++ routes.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "" => "./home.php",
+    "home" => "./home.php",
+    "services-details" => "./services-details.php",
+    "portfolio-details" => "./portfolio-details.php",
+    "404" => "./404.php",
+    "terms" => "./terms.html",
+    "privacy-policy" => "./privacy.html",
+];
